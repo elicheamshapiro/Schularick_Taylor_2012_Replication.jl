c539c835a9c4a0d53dabc5dba3d5486f8f68da12
Documenter.jl <documenter@juliadocs.github.io>
2025-01-03 00:58:49 +0000

    build based on 6ac82a0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Schularick_Taylor_2012_Replication","category":"page"},{"location":"#Schularick*Taylor*2012_Replication","page":"Home","title":"SchularickTaylor2012_Replication","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SchularickTaylor2012_Replication.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Schularick_Taylor_2012_Replication]","category":"page"}]
+}
